c7f01deddff284ace9bdd5d76dce7925dad26ffb
caregor <caregor@gmail.com>
2022-07-20 13:36:43 +0300

    Task 1 is done.

---
+++ Task_1/Program.cs
@@ -0,0 +1,64 @@
+﻿//Задача 1: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+
+int Promt(string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+
+    return result;
+}
+int[,] GenerateMatrix(int rows = 3, int columns = 4)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(1, 9);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] SortRow(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int j = 0;
+        int tmp = 0;
+        while (j < array.GetLength(1) - 1)
+        {
+            if (array[i, j] > array[i, j + 1])
+            {
+                tmp = array[i, j + 1];
+                array[i, j + 1] = array[i, j];
+                array[i, j] = tmp;
+                j = 0;
+            }
+            else
+            {
+                j++;
+            }
+        }
+    }
+    return array;
+}
+
+int[,] matix = GenerateMatrix();
+PrintArray(matix);
+
+Console.WriteLine();
+
+int[,] sorted = SortRow(matix);
+PrintArray(sorted);
